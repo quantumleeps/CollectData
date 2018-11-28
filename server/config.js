47cfa9d4b859bdc12e59ee17aa50afd6e29a5430
quantumleeps <dan.leeper@gmail.com>
2018-11-28 17:58:01 -0500

    Third time is a charm

--- server/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://dan123:dan123@ds147003.mlab.com:47003/mern-comment-box',
-  port: process.env.PORT || 8000,
-};
-
-export default config;
